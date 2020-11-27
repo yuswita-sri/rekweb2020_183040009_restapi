892be6dfbea00e4683bdd1e2af6179e5629ada81
yuswita-sri <yuswitasri@gmail.com>
2020-11-28 00:01:47 +0700

    latihan2

--- latihan1/index.php
+++ latihan1/index.php
@@ -21,9 +21,32 @@ echo $data_json;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REST API</title>
 </head>
-<link rel="stylesheet" href="">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMedM0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-<body>
+<body class="container">
+    <h3 class="my-3">Daftar Mahasiswa</h3>
+    <table border="1" class="table table-hover" cellspacing="0" cellpadding="10">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NRP</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $i = 1; ?>
+            <?php foreach (json_decode($data_json) as $data) {  ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td><?= $data[1]; ?></td>
+                    <td><?= $data[2]; ?></td>
+                    <td><?= $data[3]; ?></td>
+                </tr>
+                <?php $i++; ?>
+            <?php } ?>
+        </tbody>
+    </table>
 
 </body>
 
